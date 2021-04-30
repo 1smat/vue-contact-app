4354dec32eb438d33d623bcd1391c53390ac075d
ism4tullo <reven13022002@gmail.com>
2021-04-30 11:59:24 -0400

    done

--- client/front.js
+++ client/front.js
@@ -7,12 +7,30 @@ new Vue({
       form: {
         name: '',
         value: ''
-      }
+      },
+      contacts: [
+        {id: 1, name: 'firstName', value: '99-889-33-44', marked: false}
+      ]
+    }
+  },
+  computed: {
+    canCreate() {
+      return this.form.value.trim() && this.form.name.trim()
     }
   },
   methods: {
     createContact() {
       console.log(this.form);
-    }
+      const {...contact} = this.form
+      this.contacts.push({...contact, id: Date.now(), marked: false})
+      this.form.name = this.form.value = ''
+    },
+      markContact(id) {
+        const contact = this.contacts.find(c => c.id === id)
+        contact.marked = true
+      },
+      removeContact(id) {
+        this.contacts = this.contacts.filter(c => c.id !== id)
+      }
   }
 })
